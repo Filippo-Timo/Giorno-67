81fae678662375fa9e5d3201057b88a66f7a3ab8
Filippo-Timo <filtimo2002@gmail.com>
2026-01-20 15:29:19 +0100

    Adds a comment

--- src/main/java/filippotimo/Application.java
+++ src/main/java/filippotimo/Application.java
@@ -9,6 +9,8 @@ public class Application {
     // Per connetterci al DB dobbiamo aggiungere al main un attributo statico di tipo EntityManagerFactory che sfrutterà
     // la Persistence Unit definita in persistence.xml per connettersi al DB (dobbiamo quindi passargli come parametro il nome della PU che in questo caso è "gestioneeventipu")
 
+    // Se runnando la console esce scritto "Hello World!" allora la connsessione al server è andata a buon fine e posso committare e pushare la repo su github!!!
+
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
